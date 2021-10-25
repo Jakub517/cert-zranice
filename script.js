f8bba87f78957a4946fdcef5a0b18d6c9c51a764
Jakub <pavlu.jakub8@seznam.cz>
2021-10-25 15:18:55 +0200

    Přidána teplota

--- script.js
+++ script.js
@@ -126,6 +126,8 @@ function zkontrolujTeplotu() {
     var certuvPocit = "je Čertovi akorát";
     if (teplotaVBorsove>30){
         certuvPocit = "se Čert usmažil"
+    } else if (teplotaVBorsove<5){
+        certuvPocit = "Z Čerta je rampouch"
     } else if(teplotaVBorsove<12){
         certuvPocit = "je Čertovi zima";
     } else if (teplotaVBorsove>18){
